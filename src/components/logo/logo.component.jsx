0c82bc86fe7c597c535dc64183521902588f8516
at235am <at235am@gmail.com>
2019-10-29 06:07:45 -0700

    Initial Commit: nav bar started

---
+++ src/components/logo/logo.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./logo.styles.scss";
+
+const TempLogo = () => (
+  <div className="logo-container">
+    <div className="logo"></div>
+    <div className="logo-text">
+      broadkats<span className="suffix">.me</span>
+    </div>
+  </div>
+);
+
+export default TempLogo;
---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+// import BarsIcon from "../../assets/icons/bars-solid";
+import BarsIcon from "../icons/three-bars.icon";
+import XIcon from "../icons/x.icon";
+import PeopleIcon from "../icons/people.icon";
+import ThreeCircleIcon from "../icons/three-circles.icon";
+import PlusIcon from "../icons/plus.icon";
+
+import "./navbar.styles.scss";
+
+const NavBar = () => (
+  <div className="navbar-container">
+    <div className="logo-container">
+      <div className="logo-text">BROADKATS</div>
+
+      <div className="me-dot-container">
+        <div className="me-dot-item">me</div>
+      </div>
+    </div>
+    <div className="room-nav">
+      <div className="item-container">
+        <div className="item">
+          <ThreeCircleIcon fill="#353744" width="15" height="15" />
+        </div>
+      </div>
+      <div className="item-container">
+        <div className="item">
+          <PlusIcon fill="#353744" width="15" height="15" />
+        </div>
+      </div>
+      <div className="item-container">
+        <div className="item"></div>
+      </div>
+      <div className="item-container">
+        <div className="item"></div>
+      </div>
+      <div className="item-container">
+        <div className="item"></div>
+      </div>
+    </div>
+    <div className="site-nav">
+      <div className="page-drop-down">
+        <BarsIcon fill="#353744" width="15" height="15" />
+      </div>
+      <div className="user-settings">
+        <PeopleIcon fill="#353744" width="15" height="15" />
+      </div>
+      <div className="logout-btn">
+        <XIcon fill="#353744" width="15" height="15" />
+      </div>
+    </div>
+  </div>
+);
+
+export default NavBar;
