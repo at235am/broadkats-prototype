55ffc9b14f35a6d1d64f579237eed1d649bf74a6
at235am <at235am@gmail.com>
2019-10-31 02:54:57 -0700

    small refractor

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -11,47 +11,52 @@ import "./navbar.styles.scss";
 
 const NavBar = () => (
   <div className="navbar-container">
+    {/* LOGO: */}
     <div className="logo-container">
       <div className="logo-text">BROADKATS</div>
 
       <div className="me-dot-container">
-        <div className="me-dot-item">me</div>
+        <div className="circle">me</div>
       </div>
     </div>
+    {/* ROOM NAV */}
     <div className="room-nav">
       <div className="item-container">
-        <div className="item">
-          <NineDotIcon width="15" height="15" />
-        </div>
-      </div>
-      <div className="item-container">
-        <div className="item">
-          <PlusIcon fill="#353744" width="15" height="15" />
+        <div className="circle">
+          <NineDotIcon className="icon-properties" width="15" />
         </div>
       </div>
 
       <div className="item-container">
-        <div className="item"></div>
+        <div className="circle">
+          <SearchIcon className="icon-properties" width="15" />
+        </div>
       </div>
       <div className="item-container">
-        <div className="item"></div>
+        <div className="circle">
+          <PlusIcon className="icon-properties" width="15" />
+        </div>
       </div>
       <div className="item-container">
-        <div className="item"></div>
+        <div className="circle"></div>
       </div>
       <div className="item-container">
-        <div className="item"></div>
+        <div className="circle"></div>
+      </div>
+      <div className="item-container">
+        <div className="circle"></div>
       </div>
     </div>
+    {/* SITE NAV */}
     <div className="site-nav">
       <div className="page-drop-down">
-        <BarsIcon fill="#353744" width="15" height="15" />
+        <BarsIcon className="icon-properties" width="15" />
       </div>
       <div className="user-settings">
-        <PeopleIcon fill="#353744" width="15" height="15" />
+        <PeopleIcon className="icon-properties" width="15" />
       </div>
       <div className="logout-btn">
-        <XIcon fill="#353744" width="15" height="15" />
+        <XIcon className="icon-properties" width="15" />
       </div>
     </div>
   </div>
