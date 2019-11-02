86fa7dfe4b9dd33bcec79c109fadbea20c859b8c
Enrique Zuniga <36870131+zuniganoel@users.noreply.github.com>
2019-11-01 17:54:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="src">
+      <BrowserRouter>
         <NavBar />
         <Switch>
           <Route exact path="/login" component={LoginRegisterPage} />
