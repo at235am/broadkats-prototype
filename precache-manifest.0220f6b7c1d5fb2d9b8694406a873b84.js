4fdac2ebc7dbe3f7fb3839a883e8246e200768d9
Enrique Zuniga <36870131+zuniganoel@users.noreply.github.com>
2019-11-01 17:57:42 -0700

    Updates

---
+++ precache-manifest.0220f6b7c1d5fb2d9b8694406a873b84.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec02a077824ab4238c3c79c0ac75865d",
+    "url": "/broadkats-me-website/index.html"
+  },
+  {
+    "revision": "4fff412ad57566008675",
+    "url": "/broadkats-me-website/static/css/main.1d13a83c.chunk.css"
+  },
+  {
+    "revision": "b37708612186b80af8b7",
+    "url": "/broadkats-me-website/static/js/2.cac81cd5.chunk.js"
+  },
+  {
+    "revision": "4fff412ad57566008675",
+    "url": "/broadkats-me-website/static/js/main.d05bef16.chunk.js"
+  },
+  {
+    "revision": "0a348fd9d9db49bd9682",
+    "url": "/broadkats-me-website/static/js/runtime-main.f56eb558.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/broadkats-me-website/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "67ccea64a42da129aca02fa2fd377016",
+    "url": "/broadkats-me-website/static/media/nine-dots-solid.67ccea64.svg"
+  },
+  {
+    "revision": "b99e8ee27ac446e7f85fd8bb73d72aae",
+    "url": "/broadkats-me-website/static/media/plus-solid.b99e8ee2.svg"
+  },
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "/broadkats-me-website/static/media/search-solid.c6a0effb.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/broadkats-me-website/static/media/times-solid.2c5552a1.svg"
+  },
+  {
+    "revision": "c35809137a056d631f4d908dac3334f3",
+    "url": "/broadkats-me-website/static/media/user-circle-solid.c3580913.svg"
+  }
+]);
